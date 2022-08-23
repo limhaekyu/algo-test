aad553ba4375511e87310807e1399b8264fd10ce
limhaekyu <limk9500@gmail.com>
2022-08-23 10:05:31 +0900

    feat: solved programmers level1

---
+++ Programmers/level1/level1_같은숫자는 싫어.py
@@ -0,0 +1,14 @@
+def solution(arr):
+    answer = []
+
+    for i in range(len(arr)):
+        if len(answer) == 0:
+            answer.append(arr[i])
+
+        elif answer[len(answer) - 1] != arr[i]:
+            answer.append(arr[i])
+
+        else:
+            continue
+
+    return answer
