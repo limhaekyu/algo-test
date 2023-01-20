7ce30dee1c248e9291b6fa462e44833a7664180b
limhaekyu <limk9500@gmail.com>
2023-01-20 13:54:57 +0900

    study: 2023.01.20(coding test)

--- BackJoon/bfs/[BOJ]1260.py
+++ BackJoon/bfs/[BOJ]1260.py
@@ -1,39 +1,39 @@
+import sys
 from collections import deque
+input = sys.stdin.readline
 
-N, M, V = map(int, input().split())
 
-graph = [[0] * (N + 1) for _ in range(N + 1)]
-
-for _ in range(M):
-  m1, m2 = map(int, input().split())
-
-  graph[m1][m2] = graph[m2][m1] = 1
-
-# 너비 우선 탐색
-def bfs(start_v):
-  discoverd = [start_v]
-
-  queue = deque()
-  queue.append(start_v)
-
-  while queue:
-    v = queue.popleft()
+# dfs
+def dfs(v):
+    check1[v] = 1
     print(v, end=' ')
+    for i in range(n + 1):
+        if check1[i] == 0 and tree[v][i] == 1:
+            dfs(i)
 
-    for w in range(len(graph[start_v])):
-      if graph[v][w] == 1 and (w not in discoverd):
-        discoverd.append(w)
-        queue.append(w)
+# bfs
+def bfs(v):
+    q = deque()
+    q.append(v)
+    check2[v] = 1
 
-# 깊이 우선 탐색
-def dfs(start_v, discoverd=[]):
-  discoverd.append(start_v)
-  print(start_v, end=' ')
+    while(q):
+        v = q.popleft()
+        print(v, end=' ')
+        for i in range(n+1):
+            if check2[i] == 0 and tree[v][i] == 1:
+                q.append(i)
+                check2[i] = 1
 
-  for w in range(len(graph[start_v])):
-    if graph[start_v][w] == 1 and (w not in discoverd):
-      dfs(w, discoverd)
+n, m, v = map(int, input().split())
+tree = [[0]*(n+1) for _ in range(n+1)]
+check1 = [0]*(n+1)
+check2 = [0]*(n+1)
 
-dfs(V)
+for _ in range(m):
+    a, b = map(int, input().split())
+    tree[a][b] = tree[b][a] = 1
+
+dfs(v)
 print()
-bfs(V)
+bfs(v)
